9533f22387515d8273d774e18607b768e4e0f70e
BPB Online <41231825+bpbpublications@users.noreply.github.com>
2022-06-10 16:33:28 +0530

    By BPB Publications

---
+++ Chapter 12/sourcecode/InvokeStaticDemo (2).java
@@ -0,0 +1,20 @@
+package com.invokestaticjavamethod;
+
+public class InvokeStaticDemo {
+
+	public InvokeStaticDemo() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public  int getResult(int num1, int num2 ) {
+		int result = num1+ num2;
+		
+		return  result;
+		
+	}
+	
+	public void sayHello() {
+		
+		System.out.println( "HELLO MULESOFT");
+	}
+}
